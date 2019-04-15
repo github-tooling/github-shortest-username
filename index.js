3b839d966c144e7977997d4bab257842df1529d9
Roman Krivenko <yamadote@gmail.com>
2019-04-15 21:46:09 +0300

    write to file right away

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ require('lodash.product');
 const _ = require('lodash');
 
 (async () => {
-  const browser = await puppeteer.launch({executablePath: "/usr/bin/google-chrome", headless: false});
+  const browser = await puppeteer.launch({executablePath: "/usr/bin/google-chrome", headless: true});
   const page = await browser.newPage();
   await page.setViewport({ width: 675, height: 600 });
 
   const letters = "abcdefghijklmnopqrstuvwxyz";
   const arr_letters = letters.split('');
-  const success = [];
   const length = 4;
   let product = _.product(...Array(length).fill(arr_letters));
   let usernames = product.map(x => x.join(''));
@@ -24,10 +23,15 @@ const _ = require('lodash');
     await page.keyboard.type(i);
     await page.waitFor(3000);
     let result = await page.$('.is-autocheck-successful');
-  
+
     if (result){
-      success.push(i);
-      console.log('successful ' + i);
+      console.log('Successful: ' + i);
+      fs.appendFile('logs/successful.txt', `${i}\n`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    } else {
+	  console.log('Error: ' + i);
     }
 
     await page.keyboard.down('Control');
@@ -35,12 +39,7 @@ const _ = require('lodash');
     await page.keyboard.up('Control');
     await page.keyboard.press('Backspace');
   }
- 
+
   await browser.close();
-  
-  fs.writeFile('successful.txt', success, (err) => {
-    if (err) throw err;
-    console.log('successful username saved!');
-  });
 
 })();
